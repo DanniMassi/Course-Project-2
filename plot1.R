b1adc8f6fb8e9fb33b5b6899719b8e77539da906
Daniela Massiceti <daniela@massicon.co.za>
2014-08-09 22:50:52 +0200

    Second commit

---
+++ plot1.R
@@ -0,0 +1,7 @@
+#Coursera Exploratory Data Analysis Course Project 2
+#Author: Daniela Massiceti
+#Date: 8th August 2014
+
+#read data from files
+NEI <- readRDS("./data/summarySCC_PM25.rds")
+SCC <- readRDS("./data/Source_Classification_Code.rds")
